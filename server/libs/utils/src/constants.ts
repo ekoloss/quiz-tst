2a71881bc90e31ba239210b3ea4ddcfb9986d1de
EvgKoloskov <ekoloss.work@gmail.com>
2022-11-18 17:00:51 +0200

    config fix

--- server/libs/utils/src/constants.ts
+++ server/libs/utils/src/constants.ts
@@ -4,4 +4,9 @@ export enum namespaces {
   password = '04d43793-e0ba-53de-89ac-842d968e84bd',
 }
 
+export enum groupConstants {
+  pageLimit = 10,
+  page = 0,
+}
+
 export const defaultPageSize = 20;
